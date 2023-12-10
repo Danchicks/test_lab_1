528dbd64645c82603bc00f2b90c9de09a5ebfbf9
Danchicks <danchickst@gmail.com>
2023-12-10 20:30:42 +0200

    Add project files.

---
+++ CalcTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CalcTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CalcTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9df816bb-83fe-420f-b614-1f71a1b84cf8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CalcTests/UnitTest2.cs
@@ -0,0 +1,39 @@
+﻿using CalcClassBr;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AnalaizerClassLibrary;
+
+namespace CalcTests
+{
+
+    [TestClass]
+
+    public class SubCalcTests
+    {
+        public TestContext TestContext { get; set; }
+        [DataSource("System.Data.SqlClient", "Server = DESKTOP-NHBNNPL\\SQLEXPRESS;Integrated Security = True;Database = CalculatorData", "CalcTest", DataAccessMethod.Sequential)]
+
+        [TestMethod]
+        public void SubTest()
+        {
+            // Arrange
+            long a = Convert.ToInt64(TestContext.DataRow["a"]);
+            long b = Convert.ToInt64(TestContext.DataRow["b"]);
+            long expectedResult = Convert.ToInt64(TestContext.DataRow["r"]);
+            try
+            {
+                // Act
+                long actualResult = CalcClass.Sub(a, b);
+
+                // Assert
+                Assert.AreEqual(expectedResult, actualResult);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Test failed with exception: {ex.Message}");
+            }
+
+        }
+    }
+}
+
